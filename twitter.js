f24b8a48486bc2b1b760f484fab302ef3ca8b4ad
maccam912 <maccam912@gmail.com>
2013-01-22 18:25:27 -0600

    cleaned up stuff?

--- twitter.js
+++ twitter.js
@@ -27,16 +27,17 @@ function twitter(str) {
     for (var i = 0; i < arr.length; i++) {
         tweet(arr[i]);
     }
+    t=setTimeout(function(){twitter(user)},(3600000*arr.length)/200);
 }
 
 function tweet(user){
     var numTweets = 1;
     var url = 'http://api.twitter.com/1/statuses/user_timeline.json?callback=?&count='+numTweets+'&include_rts=1&include_entities=1&screen_name='+user;
     console.log(url);
-	$.getJSON(url,function(data){
-    $("#twitter").append('<div class="tweet">');
-    $(".tweet").last().append('<div class="user">'+user+'</div>');
-    for(var i = 0; i< data.length; i++){
+    $.getJSON(url,function(data){
+        $("#twitter").append('<div class="tweet">');
+        $(".tweet").last().append('<div class="user">'+user+'</div>');
+        for(var i = 0; i< data.length; i++){
             var tweet = data[i].text;
             var created = parseDate(data[i].created_at);
             var createdDate = created.getDate()+'-'+(created.getMonth()+1)+'-'+created.getFullYear()+' at '+created.getHours()+':'+created.getMinutes();
@@ -45,5 +46,4 @@ function tweet(user){
             $("#twitter").append('</div>');
         }
     });
-t=setTimeout(function(){twitter(user)},60000);
 }
